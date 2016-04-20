a650dc4ed86c3c2cdf56ee70d6882504d8cba235
GotGitHub <gotgithub@gmail.com>
2016-04-20 18:56:59 +0800

    add modem

---
+++ SSHE/src/main/java/me/gacl/dao/UserDaoI.java
@@ -0,0 +1,15 @@
+package me.gacl.dao;
+
+import java.io.Serializable;
+
+import me.gacl.model.User;
+
+public interface UserDaoI {
+
+    /**
+     * 保存用户
+     * @param user
+     * @return
+     */
+    Serializable save(User user); 
+}
